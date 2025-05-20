dc77887027a93af8d05a35c61b16eab32bba5f16
TertileYuta <yutarkagey@gmail.com>
2025-05-20 14:29:54 +0900

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint.config.js
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,8 +10,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "next/core-web-vitals",
+    "prettier"
+  ),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "unused-imports": require("eslint-plugin-unused-imports"),
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+    },
+  },
 ];
-
-export default eslintConfig;
